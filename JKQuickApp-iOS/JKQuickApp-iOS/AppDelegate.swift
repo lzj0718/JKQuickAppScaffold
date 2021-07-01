9447377b3a25f5bb5fe9a02e18cd443ccea363f3
Jacky <jackyliang@xiaoe-tech.com>
2021-07-01 11:47:47 +0800

    创建demo工程

---
+++ JKQuickApp-iOS/JKQuickApp-iOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  JKQuickApp-iOS
+//
+//  Created by Jacky Liang on 2021/7/1.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window : UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = ViewController()
+        return true
+    }
+
+   
+
+
+}
+
