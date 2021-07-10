4a48ba1f4f90650830521e6aae14084c77af0122
Jacky <jackyliang@xiaoe-tech.com>
2021-07-10 15:31:01 +0800

    更新安卓工程

--- JKQuickApp-Android/app/src/main/java/com/xe/jkapp/MainActivity.java
+++ JKQuickApp-Android/app/src/main/java/com/xe/jkapp/MainActivity.java
@@ -1,22 +1,35 @@
 package com.xe.jkapp;
 
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.xe.jkapp.databinding.ActivityMainBinding;
 
+import org.jetbrains.annotations.NotNull;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    /**
+     * BottomNav显示控制
+     */
+    private boolean isShowBottomNav = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +50,39 @@ public class MainActivity extends AppCompatActivity {
         //设置底部导航条
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        //设置导航颜色
+        //控制顶部bottom隐藏
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination,
+                                             @Nullable Bundle arguments) {
+                if (destination.getId() == R.id.navigation_home ||
+                        destination.getId() == R.id.navigation_dashboard ||
+                        destination.getId() == R.id.navigation_notifications) {
+                    showBottomView();
+                }else {
+                    hideBottomNav();
+                }
+            }
+        });
 
     }
+
+    private void showBottomView(){
+            if (!isShowBottomNav) {
+                binding.navView.setVisibility(View.VISIBLE);
+                ObjectAnimator.ofFloat(binding.navView, "translationY", binding.navView.getHeight(), 0).setDuration(250).start();
+                isShowBottomNav = true;
+            }
+    }
+
+    private void hideBottomNav(){
+            if (isShowBottomNav) {
+                ObjectAnimator.ofFloat(binding.navView, "translationY", 0, binding.navView.getHeight()).setDuration(250).start();
+                isShowBottomNav = false;
+            }
+    }
+
 
     @Override
     public boolean onSupportNavigateUp(){
